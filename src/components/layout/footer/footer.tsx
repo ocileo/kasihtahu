f647d0445a009d430c5980a9d78dec78ca2f3b43
crbnet <96461958+ocileo@users.noreply.github.com>
2022-07-18 04:01:18 -0700

    Update footer.tsx

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -20,6 +20,7 @@ export const Footer: FunctionComponent<Footer> = ({
   linkedin,
 }) => {
   return (
+    <div id="container-5162f63065cb14f64cf845eb0cf9ece1"></div>
     <div className="bg-white">
       <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center md:order-2">
